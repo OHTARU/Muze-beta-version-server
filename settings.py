509a595ab778e17671fe2b23b2cd2f2233da31ac
HOTARU <128468098+OHTARU@users.noreply.github.com>
2023-08-08 07:06:28 +0900

    1
    1

---
+++ settings.py
@@ -0,0 +1,15 @@
+INSTALLED_APPS = [
+    'main',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+...
+
+LANGUAGE_CODE = 'ko-kr'
+
+TIME_ZONE = 'Asia/Seoul'
