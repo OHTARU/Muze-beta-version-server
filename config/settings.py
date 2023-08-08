7ef520c69052080c0499f095d2bfb0f856740dae
Ubuntu <ubuntu@ip-172-31-45-220.ap-northeast-2.compute.internal>
2023-08-08 11:11:22 +0000

    AWS EC2

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -165,7 +165,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- config/urls.py
+++ config/urls.py
@@ -54,7 +54,7 @@ urlpatterns = [
     path(
         r"redoc", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc-v1"
     ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
